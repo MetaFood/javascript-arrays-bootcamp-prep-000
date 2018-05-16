e5c8f7e37ef6ec463befaccd44e44e679345c33f
Matthew Smyth <msmyth1030@gmail.com>
2018-05-16 02:06:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function addElementToBeginningOfArray(chocolateBars, newItem) {
+/*function addElementToBeginningOfArray(chocolateBars, newItem) {
   var newChocolateBars = [...newItem, chocolateBars]
   return newChocolateBars;
 }
 function destructivelyAddElemtnToBeginningOfArray() {
   return chocolateBars.unshift(newItem);
-}
+}*/
